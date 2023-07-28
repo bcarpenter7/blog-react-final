c130aaeb1004e781f75b7ed5da606b937453fe2f
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-07-28 11:51:41 -0400

    setup

--- Unit_4/6-simple-crud/client/src/components/Post.js
+++ Unit_4/6-simple-crud/client/src/components/Post.js
@@ -1,10 +1,16 @@
-const Post = (props) => {
+
+
+export default function Post(props) {
+    const img = props.post.img
     return (
         <>
             <h3>Name: {props.post.author}</h3>
-        
+            <h3>Title: {props.post.title}</h3>
+            <h3>Content: {props.post.content}</h3>
+             <img src={img} /> 
+            <h2>{props.post.img}</h2>
+
         </>
     )
 }
 
-export default Person
